81f9a278a4e6a5f45284a96e9afaf6fdeaa17f2e
Riya Saxena <riysaxen@amazon.com>
2024-04-26 15:33:26 -0700

    fixed email tests due to a merge conflict in backport
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>

--- public/pages/Emails/__tests__/EmailSenders.test.tsx
+++ public/pages/Emails/__tests__/EmailSenders.test.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { routerComponentPropsMock } from '../../../../test/mocks/routerPropsMock';
 import {
