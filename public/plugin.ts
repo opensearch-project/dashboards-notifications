44c64d0a9c512705c0b69de3e5d3d9fc9c946ec8
Riya Saxena <riysaxen@amazon.com>
2024-07-18 17:17:40 -0700

    navLinks changes
    Signed-off-by: Riya Saxena <riysaxen@amazon.com>

--- public/plugin.ts
+++ public/plugin.ts
@@ -6,7 +6,6 @@
 import { i18n } from '@osd/i18n';
 import {
   AppMountParameters,
-  AppCategory,
   CoreSetup,
   CoreStart,
   DEFAULT_APP_CATEGORIES,
@@ -20,15 +19,14 @@ import {
   NotificationsDashboardsSetupDeps,
 } from './types';
 import { PLUGIN_NAME } from '../common';
-import { AppPluginStartDependencies } from './types';
 import { ROUTES } from './utils/constants';
 
 export class notificationsDashboardsPlugin
   implements
-    Plugin<
-      notificationsDashboardsPluginSetup,
-      notificationsDashboardsPluginStart
-    > {
+  Plugin<
+    notificationsDashboardsPluginSetup,
+    notificationsDashboardsPluginStart
+  > {
   private title = i18n.translate('notification.notificationTitle', {
     defaultMessage: 'Notifications',
   });
@@ -41,7 +39,7 @@ export class notificationsDashboardsPlugin
     const mountWrapper = async (params: AppMountParameters, redirect: string) => {
       const { renderApp } = await import("./application");
       const [coreStart, depsStart] = await core.getStartServices();
-      return renderApp(coreStart, params, dataSourceManagement, depsStart, redirect);
+      return renderApp(coreStart, params, dataSourceManagement!, depsStart, redirect);
     };
 
     // Register an application into the side navigation menu
@@ -57,7 +55,7 @@ export class notificationsDashboardsPlugin
         // Get start services as specified in opensearch_dashboards.json
         const [coreStart, depsStart] = await core.getStartServices();
         // Render the application
-        return renderApp(coreStart, params, dataSourceManagement, depsStart, ROUTES.CHANNELS);
+        return renderApp(coreStart, params, dataSourceManagement!, depsStart, ROUTES.CHANNELS);
       },
     });
 
@@ -73,13 +71,51 @@ export class notificationsDashboardsPlugin
       });
     }
 
-    if (core.chrome.navGroup.getNavGroupEnabled()) {
+    // register applications with category and use case information
+    core.chrome.navGroup.addNavLinksToGroup(DEFAULT_NAV_GROUPS.observability, [
+      {
+        id: PLUGIN_NAME,
+        showInAllNavGroup: false
+      }
+    ])
+    core.chrome.navGroup.addNavLinksToGroup(DEFAULT_NAV_GROUPS['security-analytics'], [
+      {
+        id: PLUGIN_NAME,
+        showInAllNavGroup: false
+      }
+    ])
+    core.chrome.navGroup.addNavLinksToGroup(DEFAULT_NAV_GROUPS.analytics, [
+      {
+        id: PLUGIN_NAME,
+        showInAllNavGroup: false
+      }
+    ])
+    core.chrome.navGroup.addNavLinksToGroup(DEFAULT_NAV_GROUPS.settingsAndSetup, [
+      {
+        id: PLUGIN_NAME,
+        showInAllNavGroup: true
+      }
+    ])
+    core.chrome.navGroup.addNavLinksToGroup(DEFAULT_NAV_GROUPS.search, [
+      {
+        id: PLUGIN_NAME,
+        showInAllNavGroup: false
+      }
+    ])
+    core.chrome.navGroup.addNavLinksToGroup(DEFAULT_NAV_GROUPS.dataAdministration, [
+      {
+        id: PLUGIN_NAME,
+        showInAllNavGroup: false
+      }
+    ])
 
+    if (core.chrome.navGroup.getNavGroupEnabled()) {
       // channels route
       core.application.register({
         id: `channels`,
         title: 'Channels',
         order: 9070,
+        workspaceAvailability: WorkspaceAvailability.outsideWorkspace,
         mount: async (params: AppMountParameters) => {
           return mountWrapper(params, ROUTES.CHANNELS);
         },
@@ -88,7 +124,8 @@ export class notificationsDashboardsPlugin
       core.application.register({
         id: `email_senders`,
         title: 'Email senders',
-        order: 9070,
+        order: 9080,
+        workspaceAvailability: WorkspaceAvailability.outsideWorkspace,
         mount: async (params: AppMountParameters) => {
           return mountWrapper(params, ROUTES.EMAIL_SENDERS);
         },
@@ -97,32 +134,50 @@ export class notificationsDashboardsPlugin
       core.application.register({
         id: `email_groups`,
         title: 'Email recepient groups',
-        order: 9070,
+        order: 9090,
+        workspaceAvailability: WorkspaceAvailability.outsideWorkspace,
         mount: async (params: AppMountParameters) => {
           return mountWrapper(params, ROUTES.EMAIL_GROUPS);
         },
       });
 
+      const navLinks = [
+        {
+          id: `channels`,
+          parentNavLinkId: PLUGIN_NAME
+        },
+        {
+          id: `email_senders`,
+          parentNavLinkId: PLUGIN_NAME
+        },
+        {
+          id: `email_groups`,
+          parentNavLinkId: PLUGIN_NAME
+        },
+      ];
       core.chrome.navGroup.addNavLinksToGroup(
         DEFAULT_NAV_GROUPS.settingsAndSetup,
-        [
-          {
-            id: PLUGIN_NAME,
-          },
-          {
-            id: `channels`,
-            parentNavLinkId: PLUGIN_NAME
-          },
-          {
-            id: `email_senders`,
-            parentNavLinkId: PLUGIN_NAME
-          },
-          {
-            id: `email_groups`,
-            parentNavLinkId: PLUGIN_NAME
-          },
-      
-        ]
+        navLinks
+      );
+      core.chrome.navGroup.addNavLinksToGroup(
+        DEFAULT_NAV_GROUPS.search,
+        navLinks
+      );
+      core.chrome.navGroup.addNavLinksToGroup(
+        DEFAULT_NAV_GROUPS['security-analytics'],
+        navLinks
+      );
+      core.chrome.navGroup.addNavLinksToGroup(
+        DEFAULT_NAV_GROUPS.analytics,
+        navLinks
+      );
+      core.chrome.navGroup.addNavLinksToGroup(
+        DEFAULT_NAV_GROUPS.dataAdministration,
+        navLinks
+      );
+      core.chrome.navGroup.addNavLinksToGroup(
+        DEFAULT_NAV_GROUPS.observability,
+        navLinks
       );
     }
 
@@ -134,5 +189,5 @@ export class notificationsDashboardsPlugin
     return {};
   }
 
-  public stop() {}
+  public stop() { }
 }
